f20e96ee019dbce352b1931cb2d0c4a5e5595a2b
danielaaron <danielaaron@users.noreply.github.com>
2016-03-13 18:29:57 -0700

    Update solver_clean.py

--- solver_clean.py
+++ solver_clean.py
@@ -1,28 +1,6 @@
 import sys
 from userfile import initial_position, primitive, gen_moves, do_moves
 
-"""
-Data Types (f: input --> output):
-initial_position: string
-primitive(pos): string --> "win", "loss", "tie"
-gen_moves(pos): string --> list
-do_moves(pos, move): string --> string 
-
-Notes:
-> Game states are always strings
-> gen_moves must generate all possible
-  choices of the opponent and return only the 
-  gamestates in which it's the user's move
-> This solver can't handle looping games
-
-User Input: 
-To input the three given functions and the initial position,
-users should write a userfile.py, in the same directory as
-this file, containing the four given user inputs. 
-"""
-
-
-
 def solve(parents):
 	children = []
 	
